1ec09d972f2f44d31013255ad2a6959fd5868925
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-02-25 14:53:35 -0800

    adding stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
+
 import Card from "./components/Card/"
 import "./styles/App.css"
 import Header from "./components/Header";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [people, setPeople] = useState(["Joe","Carter","Nile","Chrissie"])
+  const buttonClick = e=>{
+    setCounter(counter+1)
+  }
+  const resetCount = e=>{
+    setCounter(0)
+  }
+
   return (
     <div className="App">
       <Header/>
       <h1>Welcome!</h1>
-      <h3>members</h3>
-      <Card name="Joe" description="best teacher ever"/>
-      <Card name="Carter"/>
-      <Card name="Nile"/>
-      <Card/>
-      <Card/>
+      <button onClick={buttonClick}>Clicks: {counter}</button>
+      <button onClick={resetCount}>reset count</button>
+      <h3>There are {people.length} members!</h3>
+      {people.map(person=><Card name={person}/>)}
     </div>
   );
 }
